6195b08491d4ece6a320206fa321694cb3db3292
Erin Michelle <emw337@drexel.edu>
2018-02-13 15:46:30 -0500

    Initial Commit

---
+++ wk06buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wk06buttons
+//
+//  Created by Erin Wiegman on 2/13/18.
+//  Copyright © 2018 Erin Wiegman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
