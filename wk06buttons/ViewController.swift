4aa593590bb2e4a0aec9ea11aff31cf88d9d6792
Erin Michelle <emw337@drexel.edu>
2018-02-13 16:57:32 -0500

    changes on click

--- wk06buttons/ViewController.swift
+++ wk06buttons/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var toggleFlag: Bool = true
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBAction func buttonFun(_ sender: Any) {
+        
+        if (toggleFlag == true) {
+            //change color on click
+            button.tintColor = UIColor.init(named: "ok")
+            //change image on click - use this for checked
+            button.setBackgroundImage(UIImage(named: "user_group_man_woman"), for: .normal)
+            toggleFlag = false
+        } else {
+            button.tintColor = UIColor.init(named: "wine")
+            button.setBackgroundImage(UIImage(named: "people"), for: .normal)
+            toggleFlag = true
 
+        }
+//       alert pops up on button click
+        let myAlert = UIAlertController(title: "floor preference", message: "my floor is?", preferredStyle: .alert)
+        myAlert.addAction(UIAlertAction(title: "carpet", style: .default, handler: nil))
+        myAlert.addAction(UIAlertAction(title: "wood", style: .cancel, handler: nil))
+        self.present(myAlert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
